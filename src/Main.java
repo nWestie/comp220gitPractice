433e84e5e95e58fc3c3c79b94ede9c2fabeb2540
GILLETTERM21 <gilletterm21@gcc.edu>
2023-02-27 13:47:27 -0500

    Added Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner yourName = new Scanner(System.in);
+        System.out.println("Yo, what's ur name, dude?");
+        String dudesName = yourName.next();
+        System.out.println("GOODBYE " + dudesName);
     }
 }
